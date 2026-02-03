1bb704d4958c9fbe8039f0410b7bfb37a241e80e
soulware <81992759+soulware1@users.noreply.github.com>
2026-02-03 15:52:04 -0600

    joker+ slop

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -1,7 +1,12 @@
 return {
     descriptions = {
         Joker = {
-      
+            j_smallpox_examplejoker = {
+                name = 'Joker+',
+                text = {
+                    "{C:mult}+#1#{} Mult"
+                }
+            },
         },
     }
 }
