19c132f419dc1c80883c5705b67aa72badd67442
Typ0Codes <137425228+Typ0Codes@users.noreply.github.com>
2026-02-20 16:53:20 -0700

    jank main menu card

--- main.lua
+++ main.lua
@@ -140,6 +140,38 @@ function Game:main_menu(change_context)
 
 end
 
+local XmainMenuHook = Game.main_menu
+function Game:main_menu(ctx)
+    local r = XmainMenuHook(self,ctx)
+    if self.title_top then
+        local tg = self.title_top
+        local card = Card(tg.T.x,tg.T.y,G.CARD_W,G.CARD_H,nil,G.P_CENTERS['j_smallpox_smallpox'])
+        card.bypass_discovery_center = true
+        card.T.w = card.T.w * 1.4
+        card.T.h = card.T.h * 1.4
+        card:set_sprites(card.config.center)
+        card.no_ui = true
+        card.states.visible = false
+        self.title_top:emplace(card)
+        self.title_top:align_cards()
+        G.E_MANAGER:add_event(
+            Event{
+                delay = 0.5,
+                func = function ()
+                    if ctx == "splash" then
+                        card.states.visible = true
+                        card:start_materialize({ G.C.WHITE, G.C.WHITE }, true, 0.5)
+                    else
+                        card.states.visible = true
+                        card:start_materialize({ G.C.WHITE, G.C.WHITE }, nil, 0.2)
+                    end
+                    return true
+                end
+            }
+        )
+    end
+end
+
 if SPOX_CONFIG.SPOX_Startup_Message then
     
     local mm = Game.main_menu
