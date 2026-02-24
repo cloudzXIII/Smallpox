32640bd9d12dad2c01e40571952637b92c3eee02
M0x3s <138784960+M0x3s@users.noreply.github.com>
2026-02-24 00:56:40 -0500

    Fixed Menu Joker
    made it use the proper selling context rather than generic removal

--- src/jokers/restaurantmenu.lua
+++ src/jokers/restaurantmenu.lua
@@ -16,16 +16,15 @@ SMODS.Joker {
     discovered = true,
     atlas = 'Inky',
     pools = {["Smallpox"] = true},
-
-    
-    
-    remove_from_deck = function(self, card, from_debuff)
+    calculate = function(self, card, context)
         --from cryptid
-        local card = create_card("Food", G.jokers, nil, nil, nil, nil, nil)
-        card:add_to_deck()
-        card:start_materialize()
-        G.jokers:emplace(card)
-        play_sound('generic1', 0.9 + math.random()*0.1, 0.8)
+        if context.selling_self then
+            local card = create_card("Food", G.jokers, nil, nil, nil, nil, nil)
+            card:add_to_deck()
+            card:start_materialize()
+            G.jokers:emplace(card)
+            play_sound('generic1', 0.9 + math.random()*0.1, 0.8)
+        end
     end,
     smallpox_credits = {
 		{
