23de454fa2d2c4278fdec52de00a9eec7efffa80
M0x3s <138784960+M0x3s@users.noreply.github.com>
2026-02-28 04:10:24 -0500

    Forgot to Put Local Variable for Press F

--- src/jokers/os.JOKER.lua
+++ src/jokers/os.JOKER.lua
@@ -154,6 +154,7 @@ SMODS.Joker {
         local s = get_system_state()
 
         if context.press_play then
+            local chips = 0
             if G.YOUAREANIDIOT.pressed_f then
                 chips = chips +
                     math.floor(G.GAME.dollars / 5) * e.f_chips
